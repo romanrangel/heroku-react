5b39a84db154246838b3ac640eb593b07ed0faf7
Roman Rangel <75048609+romanrangel@users.noreply.github.com>
2021-02-22 11:59:13 -0800

    Add files via upload

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <Link className="navbar-brand" to="/">
+        React Reading List
+      </Link>
+    </nav>
+  );
+}
+
+export default Nav;
